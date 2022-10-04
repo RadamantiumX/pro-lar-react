d9350e69379261c5341ec8228f1aa085a1b3f468
loadEdar <eduardo.arede@gmail.com>
2022-10-04 20:58:06 -0300

    se completa migracion

--- database/migrations/2022_10_04_234857_create_comentarios_table.php
+++ database/migrations/2022_10_04_234857_create_comentarios_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('comentarios', function (Blueprint $table) {
-            $table->id();
+            $table->string('email')->primary();
+            $table->string('celular');
+            $table->string('comentario');
             $table->timestamps();
         });
     }
