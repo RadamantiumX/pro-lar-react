0a0d57cf214b01c906e2646d732c7cb321b7c607
loadEdar <eduardo.arede@gmail.com>
2022-10-06 21:40:27 -0300

    cambios varios

--- app/Http/Controllers/ComentariosController.php
+++ app/Http/Controllers/ComentariosController.php
@@ -3,27 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Models\Comentario;
-use Illuminate\Support\Facades\DB;
 
 class ComentariosController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-
-
         $request->validate([
             'email' => 'required',
             'celular' => 'required|min:8',
             'comentario'=>'required|max:500'
         ]);
 
-        $emailValidate = $request->input('email');
 
-        $verify = DB::table('comentarios')->where('email', $emailValidate)->exists();
 
-        if(!$verify)
-        {
+
+
+
         $comentario = new Comentario();
         $comentario->email = $request->email;
         $comentario->celular = $request->celular;
@@ -31,15 +54,50 @@ class ComentariosController extends Controller
 
         $comentario->save();
         return redirect()->route('home')->with('success','Mensaje enviado!');
-        }else{
-            return redirect()->route('home')
-            ->with('error','El email ingresado ya ha creado una solicitud');
-        }
-
     }
 
-    public function closeForm()
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
     {
+        //
+    }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
